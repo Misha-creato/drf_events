7596c0d3101dcb4442119fac53757106bac974d4
Iri O <oparina.iri@gmail.com>
2024-08-26 19:27:55 +0500

    Added EventSerializer

---
+++ apps/events/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+
+from events.models import Event
+
+
+class EventSerializer(serializers.ModelSerializer):
+    city = serializers.SerializerMethodField()
+    place = serializers.SerializerMethodField()
+    address = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
+
+    def get_city(self, obj):
+        return obj.area.city
+
+    def get_place(self, obj):
+        return obj.area.name
+
+    def get_address(self, obj):
+        return obj.area.address
+
+    def get_category(selfs, obj):
+        return obj.category.name
+
+    class Meta:
+        model = Event
+        fields = [
+            'name',
+            'city',
+            'place',
+            'address',
+            'start_at',
+            'end_at',
+            'age_limit',
+            'category',
+            'min_price',
+            'quantity',
+            'available_tickets',
+            'description',
+        ]
