c017fa271292ab604ec9a9a9681b95c903f223c7
Iri O <oparina.iri@gmail.com>
2024-08-27 20:31:18 +0500

    Added get_event, docstrings

--- apps/events/services.py
+++ apps/events/services.py
@@ -12,6 +12,31 @@ logger = get_logger(__name__)
 
 
 def get_all_events(request: Any, filter_backends: list, view: Any) -> (int, list):
+    '''
+    Получение списка всех мероприятий
+
+    Returns:
+        Код статуса и список данных
+        200,
+        [
+            {
+                "name": "Testing test2 event",
+                "slug": "testing-test2-event",
+                "city": "Astana",
+                "place": "Test2",
+                "address": "test2 address",
+                "start_at": "2024-08-28T00:00:00+05:00",
+                "end_at": "2024-08-29T04:00:00+05:00",
+                "age_limit": 0,
+                "category": "Театр",
+                "min_price": 4000,
+                "quantity": 50,
+                "available_tickets": 45,
+                "description": "Description"
+            },
+            ....
+        ]
+    '''
     logger.info(
         msg='Получение списка активных мероприятий',
     )
@@ -46,5 +71,48 @@ def get_all_events(request: Any, filter_backends: list, view: Any) -> (int, list
     ).data
     logger.info(
         msg='Успешно получен список активных мероприятий',
+    )
+    return 200, response_data
+
+
+def get_event(slug: str) -> (int, dict):
+    '''
+    Получение списка мероприятия по слагу
+
+    Returns:
+        Код статуса и словарь данных
+        200,
+        {
+            "name": "Testing test2 event",
+            ....
+        },
+    '''
+    logger.info(
+        msg=f'Получение мероприятия по слагу {slug}',
+    )
+
+    try:
+        event = Event.objects.filter(
+            canceled=False,
+            start_at__gte=timezone.now(),
+            slug=slug,
+        ).first()
+    except Exception as exc:
+        logger.error(
+            msg=f'Возникла ошибка при получении мероприятия по слагу {slug}: {exc}',
+        )
+        return 500, {}
+
+    if event is None:
+        logger.info(
+            msg=f'Мероприятие по слагу {slug} не найдено',
+        )
+        return 404, {}
+
+    response_data = EventSerializer(
+        instance=event,
+    ).data
+    logger.info(
+        msg=f'Успешно найдено мероприятие по слагу {slug}',
     )
     return 200, response_data
