2bfa526061a88206d869c75de068d949c42d7128
Iri O <oparina.iri@gmail.com>
2024-08-27 20:30:06 +0500

    Added EventView, swagger

--- apps/events/api.py
+++ apps/events/api.py
@@ -1,3 +1,7 @@
+from drf_spectacular.utils import extend_schema
+
+from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.filters import (
@@ -5,7 +9,19 @@ from rest_framework.filters import (
     OrderingFilter,
 )
 
-from events.services import get_all_events
+
+from events.filters import EventFilter
+from events.services import (
+    get_all_events,
+    get_event,
+)
+from events.doc import (
+    event_list_parameters,
+    DefaultEventResponse,
+    EventList200Response,
+    Event200Response,
+)
+
 from utils.response_patterns import generate_response
 
 
@@ -13,6 +29,11 @@ class EventListView(APIView):
     filter_backends = [
         SearchFilter,
         OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    search_fields = [
+        'name',
+        'area__name',
     ]
     ordering_fields = [
         'name',
@@ -20,12 +41,55 @@ class EventListView(APIView):
         'start_at',
         'end_at',
     ]
+    filter_fields = [
+        'area__name',
+        'start_at',
+        'end_at',
+        'age_limit',
+        'category__name',
+        'area__city',
+    ]
+    filterset_class = EventFilter
 
+    @extend_schema(
+        parameters=event_list_parameters,
+        responses={
+            200: EventList200Response,
+            500: DefaultEventResponse,
+        },
+        description=EventList200Response.__doc__,
+        summary='Получение списка всех мероприятий',
+    )
     def get(self, request):
         status_code, response_data = get_all_events(
             request=request,
             filter_backends=self.filter_backends,
             view=self,
+        )
+        status, data = generate_response(
+            status_code=status_code,
+            data=response_data,
+        )
+        return Response(
+            status=status,
+            data=data,
+        )
+
+
+class EventView(APIView):
+
+    @extend_schema(
+        responses={
+            200: Event200Response,
+            404: DefaultEventResponse,
+            500: DefaultEventResponse,
+        },
+        description=Event200Response.__doc__,
+        summary='Получение мероприятия по слагу',
+    )
+    def get(self, request, slug):
+        status_code, response_data = get_event(
+            slug=slug,
         )
         status, data = generate_response(
             status_code=status_code,
