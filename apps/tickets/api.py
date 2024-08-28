13154289c24bc0de0cdf76bad21d4079271b4551
Iri O <oparina.iri@gmail.com>
2024-08-28 19:43:23 +0500

    Added TicketView

---
+++ apps/tickets/api.py
@@ -0,0 +1,39 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from tickets.services import get_all_tickets, check_ticket_qr
+from utils.response_patterns import generate_response
+
+
+class TicketView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        status_code, response_data = get_all_tickets(
+            user=user,
+        )
+        status, data = generate_response(
+            status_code=status_code,
+            data=response_data,
+        )
+        return Response(
+            status=status,
+            data=data,
+        )
+
+    def post(self, request):
+        data = request.data
+        status_code, response_data = check_ticket_qr(
+            data=data,
+        )
+        status, data = generate_response(
+            status_code=status_code,
+            data=response_data,
+        )
+        return Response(
+            status=status,
+            data=data,
+        )
