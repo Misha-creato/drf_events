8a4cd5ddac752af1747a9090f628ee5b91e7e6f5
Iri O <oparina.iri@gmail.com>
2024-08-28 19:44:03 +0500

    Added Ticket model

---
+++ apps/tickets/models.py
@@ -0,0 +1,82 @@
+import uuid
+
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from events.models import Landing
+
+from utils.constants import TICKET_STATUSES
+
+
+User = get_user_model()
+
+
+class Ticket(models.Model):
+    uuid = models.UUIDField(
+        verbose_name='Идентификатор',
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    landing = models.ForeignKey(
+        verbose_name='Посадка',
+        to=Landing,
+        on_delete=models.SET_NULL,
+        related_name='tickets',
+        null=True,
+    )
+    user = models.ForeignKey(
+        verbose_name='Пользователь',
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='tickets',
+    )
+    event_name = models.CharField(
+        verbose_name='Наименование мероприятия',
+        blank=True,
+    )
+    section = models.CharField(
+        verbose_name='Секция',
+        max_length=64,
+        null=True,
+        blank=True
+    )
+    row = models.CharField(
+        verbose_name='Ряд',
+        max_length=64,
+        null=True,
+        blank=True
+    )
+    price = models.DecimalField(
+        verbose_name='Цена за место',
+        max_digits=7,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+    seat = models.CharField(
+        verbose_name='Место',
+        max_length=32,
+    )
+    status = models.CharField(
+        verbose_name='Статус использования',
+        choices=TICKET_STATUSES,
+        default=TICKET_STATUSES[0],
+    )
+    bought_at = models.DateTimeField(
+        verbose_name='Дата и время покупки',
+        auto_now_add=True,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.price:
+            self.section = self.landing.section
+            self.row = self.landing.row
+            self.price = self.landing.price
+            self.event_name = self.landing.event.name
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table = 'tickets'
+        verbose_name = 'Билет'
+        verbose_name_plural = 'Билеты'
