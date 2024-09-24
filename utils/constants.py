e8f7d38fa76782155173745ef8b37d899d228731
Iri O <oparina.iri@gmail.com>
2024-09-24 18:15:58 +0500

    Added constants for email_types

--- utils/constants.py
+++ utils/constants.py
@@ -1,10 +1,15 @@
 CONFIRM_EMAIL = 'confirm_email'
 PASSWORD_RESTORE = 'password_restore'
-
+NOTIFY_3_DAYS = 'notify_3_days'
+NOTIFY_DAY_IN_DAY = 'notify_day_in_day'
+NOTIFY_EXPIRED = 'notify_expired'
 
 EMAIL_TYPES = (
     (CONFIRM_EMAIL, 'Подтверждение адреса электронной почты'),
     (PASSWORD_RESTORE, 'Восстановление пароля'),
+    (NOTIFY_DAY_IN_DAY, 'Оповещение день в день'),
+    (NOTIFY_3_DAYS, 'Оповещенеи за 3 дня'),
+    (NOTIFY_EXPIRED, 'Оповещение о просроченном билете'),
 )
 
 CITIES = (
@@ -31,9 +36,9 @@ TICKET_STATUSES = (
 
 
 NOTIFICATION_STATUSES = (
-    ('no notify', 'Не оповещен'),
-    ('3 days', 'За 3 дня'),
-    ('day in day', 'День в день'),
+    ('no_notify', 'Не оповещен'),
+    ('3_days', 'За 3 дня'),
+    ('day_in_day', 'День в день'),
     ('expired', 'Просроченный'),
 )
 
