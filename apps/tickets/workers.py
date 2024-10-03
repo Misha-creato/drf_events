7e08bb7d35c4148229f7c6deec49af1ca8bb5664
Iri O <oparina.iri@gmail.com>
2024-10-03 18:38:21 +0500

    Added check_bill_status, check_payment_status

--- apps/tickets/workers.py
+++ apps/tickets/workers.py
@@ -2,11 +2,11 @@ import datetime
 
 from django.utils import timezone
 
-from events.models import Event
-
+from tickets.services import Payment
 from tickets.tasks import notify_users
 from tickets.models import Ticket
 
+from utils import redis_cache
 from utils.logger import get_logger
 from utils.constants import (
     TICKET_STATUSES,
@@ -17,6 +17,7 @@ from utils.constants import (
 
 
 logger = get_logger(__name__)
+payment = Payment()
 
 
 def user_event_notification(notification_status: str) -> None:
@@ -131,3 +132,79 @@ def update_ticket_status() -> None:
     logger.info(
         msg=f'Обновлены статусы просроченных билетов',
     )
+
+
+def check_bill_status() -> None:
+    '''
+    Проверка статусов счетов для оплаты из списка redis
+
+    Returns:
+    None
+    '''
+
+    logger.info(
+        msg=f'Проверка статусов счетов для оплаты из списка redis',
+    )
+
+    status, bills = redis_cache.get_list(
+        key='bills_to_check',
+    )
+    if status != 200:
+        logger.error(
+            msg=f'Возникла ошибка при проверку статусов счетов для оплаты',
+        )
+        return
+
+    for bill in bills:
+        status, data = payment.confirm_buying(
+            bill_id=bill,
+        )
+        if status != 500:
+            redis_cache.remove_from_list(
+                key='bills_to_check',
+                value=bill,
+            )
+
+
+def check_payment_status() -> None:
+    '''
+    Проверка статусов платежей в ожидании
+
+    Returns:
+    None
+    '''
+
+    logger.info(
+        msg=f'Проверка статусов платежей в ожидании',
+    )
+
+    try:
+        tickets = Ticket.objects.filter(
+            status='waiting',
+        )
+    except Exception as exc:
+        logger.error(
+            msg=f'Возникла ошибка при проверке статусов платежей в ожидании'
+        )
+        return
+
+    for ticket in tickets:
+        status, response = payment.check_payment(
+            payment_id=ticket.payment_id,
+        )
+        if status == 200:
+            ticket.status = TICKET_STATUSES[1]
+            try:
+                ticket.save()
+            except Exception as exc:
+                logger.error(
+                    msg=f'Возникла ошибка при смене статуса оплаченного билета '
+                        f'{ticket}: {exc}',
+                )
+        elif status == 400:
+            try:
+                ticket.delete()
+            except Exception as exc:
+                logger.error(
+                    msg=f'Возникла ошибка при удалении неоплаченного билета {exc}'
+                )
