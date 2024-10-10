82f80060f686c14faab046942aac5af6c42a6c6f
Iri O <oparina.iri@gmail.com>
2024-10-10 19:58:35 +0500

    Updated run_tickets command

--- apps/tickets/management/commands/run_tickets.py
+++ apps/tickets/management/commands/run_tickets.py
@@ -5,6 +5,8 @@ from django.core.management.base import BaseCommand
 from tickets.workers import (
     user_event_notification,
     update_ticket_status,
+    check_bill_status,
+    check_payment_status,
 )
 
 from utils.logger import get_logger
@@ -14,13 +16,15 @@ logger = get_logger(__name__)
 
 
 class Command(BaseCommand):
-    help = 'Запускать воркеры каждую минуту'
+    help = 'Запускать воркеры билетов каждую минуту'
 
     def handle(self, *args, **kwargs):
 
         while True:
-            user_event_notification(notification_status='day_in_day')
-            user_event_notification(notification_status='3_days')
-            user_event_notification(notification_status='expired')
-            update_ticket_status()
+            # user_event_notification(notification_status='day_in_day')
+            # user_event_notification(notification_status='3_days')
+            # user_event_notification(notification_status='expired')
+            # update_ticket_status()
+            # check_bill_status()
+            check_payment_status()
             time.sleep(60)
