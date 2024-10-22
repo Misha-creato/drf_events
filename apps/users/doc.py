dfa1f10c3ee91fb19159780bd6b18765521b3e2e
Iri O <oparina.iri@gmail.com>
2024-10-22 18:15:36 +0500

    Added GoogleAuth200Response for swagger

--- apps/users/doc.py
+++ apps/users/doc.py
@@ -31,6 +31,26 @@ class Auth200Response(DefaultResponse):
     )
 
 
+class GoogleAuth200Response(DefaultResponse):
+    '''
+    Получение ссылки для авторизации пользователя через Google
+
+    '''
+
+    data = serializers.JSONField(
+        default={
+            "google_auth_url": "https://accounts.google.com/o/oauth2/v2/auth?"
+                               "scope=https%3A//www.googleapis.com/auth/drive.metadata.readonly&"
+                               "access_type=offline&"
+                               "include_granted_scopes=true&"
+                               "response_type=code&"
+                               "state=state_parameter_passthrough_value&"
+                               "redirect_uri=https%3A//oauth2.example.com/code&"
+                               "client_id=client_id"
+        }
+    )
+
+
 class RefreshToken200Response(DefaultResponse):
     '''
     Обновление токена пользователя
